1aee203383652e749f63acbeca9e15e788b0883f
Liam <90276819+liam-fletcher1@users.noreply.github.com>
2021-11-27 18:08:48 -0500

    updated dice game

--- DiceGame.java
+++ DiceGame.java
@@ -3,51 +3,80 @@
  *
  * @author  Liam Fletcher
  * @version 1.0
- * @since   2021-11-23
+ * @since   2021-11-27
  */
 
 import java.util.Scanner;
 
-public final class DiceGame {
-    private DiceGame() {
+/**
+ * The guessing game program
+ */
+final class Main {
 
-    }
     /**
-     * The guessing game program
+     * Throw an exception IllegalStateException.
+     *
+     * @throws IllegalStateException
+     *
+     */
+    private Main() {
+        throw new IllegalStateException("Invaild Input");
+    }
+
+    /**
+     * The main() function.
+     *
+     * @param args No args will be used
      */
     public static void main(final String[] args) {
 
-        // The guess counter
-        int Guesses = 0;
+        // Variables
+        int guess;
 
+        // Constants
+        final int lowestNum = 1;
+        final int highestNum = 6;
+        final int extra = 1;
+
+        // Generate random number 
+        final int randomNumb = (int)(Math.random() * (highestNum -
+            lowestNum + extra) + lowestNum);
+
+        // Create scanner objects 
+        final Scanner myObjOne = new Scanner(System.in);
+        // Ask for inputs
+        System.out.println("Guess a number between 1 and 6: ");
+        guess = myObjOne.nextInt();
+        /*
+         * This accepts an inputted
+         */
         try {
-            // Random Number
-            final int randomNum = (int)(Math.random() * (6 - 1)) + 1;
+            do {
 
-            // Process
-            while (true) {
-                // User Input
-                Scanner guess = new Scanner(System.in);
-                System.out.println("Guess a number between 1 and 6");
-                int Guess = guess.nextInt();
-
-                // Guess counter
-                Guesses = Guesses + 1;
-
-                if (Guess == randomNum) {
-                    // Output if correct
-                    System.out.println("Correct, it took you " +
-                        Guesses + " tries.");
-                    break;
+                // Calculate
+                if (guess < lowestNum || guess > highestNum) {
+                    System.out.println("Your guess won't count. Guess again.");
+                } else if (guess < randomNumb) {
+                    System.out.println("You guessed too low. Guess again.");
+                } else if (guess > randomNumb) {
+                    System.out.println("You guessed too high. Guess again.");
                 } else {
-                    // Output if wrong 
-                    System.out.println("Wrong, guess again");
+                    System.out.println("Invaild Input");
+                    break;
                 }
-            }
 
-            // Catches invaild inputs
-        } catch (Exception e) {
-            System.out.println("Invalid Input");
+                //Input
+                System.out.println("Guess a number between 1 to 6: ");
+                guess = myObjOne.nextInt();
+
+            } while (guess != randomNumb);
+
+            // Output
+            System.out.println("You guessed correctly!");
+        } catch (java.util.InputMismatchException ex) {
+            System.out.println("Invaild Input");
+        } finally {
+            System.out.println("\nDone.");
         }
     }
 }
